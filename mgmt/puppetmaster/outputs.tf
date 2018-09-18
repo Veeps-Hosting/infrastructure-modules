96a0dad2f74cd39f348c8a3c5d4eeb393d51073f
Grant Davies <grant@Grants-MacBook-Pro.local>
2018-09-18 12:07:50 +1000

    .

--- mgmt/puppetmaster/outputs.tf
+++ mgmt/puppetmaster/outputs.tf
@@ -2,14 +2,10 @@ output "dns_name" {
   value = "${aws_route53_record.puppetmaster.fqdn}"
 }
 
-output "puppetmaster_public_ip" {
-  value = "${module.bastion.public_ip}"
-}
-
 output "puppetmaster_private_ip" {
-  value = "${module.bastion.private_ip}"
+  value = "${module.puppetmaster.private_ip}"
 }
 
 output "puppetmaster_security_group_id" {
-  value = "${module.bastion.security_group_id}"
+  value = "${module.puppetmaster.security_group_id}"
 }

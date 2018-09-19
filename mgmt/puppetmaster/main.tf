3e503320d93cef8d86b19bef88efba06b41049fc
Grant Davies <grant.davies@webqem.com>
2018-09-19 15:32:54 +1000

    .

--- mgmt/puppetmaster/main.tf
+++ mgmt/puppetmaster/main.tf
@@ -10,10 +10,10 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/user-data/user-data.txt")}"
 }
 module "puppetmaster" {
-  #allow_ssh_from_security_group_id      = "${var.bastion_host_security_group_id}"
+  allow_ssh_from_security_group_id      = "${var.bastion_host_security_group_id}"
   allow_ssh_from_cidr                   = false
   allow_ssh_from_security_group         = true
-  allow_ssh_from_security_group_id      = "sg-0f62a35785364a066"
+  #allow_ssh_from_security_group_id      = "sg-0f62a35785364a066"
   ami                                   = "${var.ami}"
   attach_eip                            = false
   instance_type                         = "${var.instance_type}"

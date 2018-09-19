e1d1a381c48d861c7b3141f38c504eef7209038f
Grant Davies <grant@veepshosting.com>
2018-09-20 09:27:36 +1000

    .

--- mgmt/puppetmaster/main.tf
+++ mgmt/puppetmaster/main.tf
@@ -83,4 +83,12 @@ data "terraform_remote_state" "bastion_host" {
     bucket = "${var.terraform_state_s3_bucket}"
     key    = "${var.aws_region}/${var.vpc_name}/bastion-host/terraform.tfstate"
   }
+  data "terraform_remote_state" "sns_region" {
+    backend = "s3"
+    config {
+      region = "${var.terraform_state_aws_region}"
+      bucket = "${var.terraform_state_s3_bucket}"
+      key    = "${var.aws_region}/_global/sns-topics/terraform.tfstate"
+    }
+  }
 }
